28bc01b276f9c9301f224820149799edc9a4bb99
Alexander Rashed <alexander.rashed@gmail.com>
2016-10-05 13:38:59 +0200

    Speedtest fixes

--- monitoring/speedtest.sh
+++ monitoring/speedtest.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+echo -e 'Date\tPing ms\tUp MBit\tDown MBit'
 while true
 do
-	echo -ne $(date) '\t' && speedtest-cli --simple | grep -Eo "[0-9]*\.[0-9]*" | tr '\n' '\t'
+	# Print the current date, a tab and then the 
+	echo -ne $(date) '\t' && speedtest-cli --simple | grep -Eo "[0-9]+\.[0-9]+" | tr '\n' '\t'
+	echo ''
 	sleep 15m
 done
 

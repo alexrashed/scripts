d05224eb304b71e8dd7603a37a8f23098b7f3f70
Alexander Rashed <alexander.rashed@gmail.com>
2016-10-10 09:41:33 +0200

    Initial speedtest monitoring data and first lines on R data evaluation

---
+++ monitoring/evaluate_speedtest.r
@@ -0,0 +1,18 @@
+library(zoo)
+
+# Load the CSV Data (containing time, ping, download/upload)
+dat <- read.table("speedtest.csv", header=T)
+
+# Order the data by date
+temp <- zoo(x = dat, order.by = dat[,1])
+
+# Create the rolling mean value with w=10 for the down-/upload
+ma <- rollmean(temp, k=10, align="right")
+
+# Plot the data
+#plot(temp, screens=1, type="l", lwd=c(1,1),col=c("red2", "blue"), xlab="", ylab="MBit/s",
+#     main="UPC internet speed")
+#legend('topright', names(temp),lty=1, col=c(red2", "blue"))
+
+# Calculate the Downtime Percentage
+# Calculate the mean, lowest, highest ping/down-/upload
